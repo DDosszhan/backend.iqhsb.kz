023ca038f00e295f5168248daaeb1658e077cfe3
Anastas Mironov <ast@l80.ru>
2022-06-18 01:25:41 +0900

    feat: group admin routes with prefix iqanatcp

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,16 @@ use App\Http\Controllers\Admin\FaqController;
 |
 */
 
-Route::group(['prefix' => 'faqs'], function () {
-    Route::get('/', [FaqController::class, 'index'])->name('admin.faqs.index');
-    Route::get('/list', [FaqController::class, 'list'])->name('admin.faqs.list');
-    Route::get('/create', [FaqController::class, 'create'])->name('admin.faqs.create');
-    Route::post('/store', [FaqController::class, 'store'])->name('admin.faqs.store');
-    Route::get('/{id}/edit', [FaqController::class, 'edit'])->name('admin.faqs.edit');
-    Route::post('/{id}/update', [FaqController::class, 'update'])->name('admin.faqs.update');
-    Route::get('/{id}/delete', [FaqController::class, 'delete'])->name('admin.faqs.delete');
-    Route::get('/{id}/up', [FaqController::class, 'positionUp'])->name('admin.faqs.up');
-    Route::get('/{id}/down', [FaqController::class, 'positionDown'])->name('admin.faqs.down');
+Route::group(['prefix' => config('project.admin_prefix'), 'middleware' => ['web', 'adminMiddleware']], function () {
+    Route::group(['prefix' => 'faqs'], function () {
+        Route::get('/', [FaqController::class, 'index'])->name('admin.faqs.index');
+        Route::get('/list', [FaqController::class, 'list'])->name('admin.faqs.list');
+        Route::get('/create', [FaqController::class, 'create'])->name('admin.faqs.create');
+        Route::post('/store', [FaqController::class, 'store'])->name('admin.faqs.store');
+        Route::get('/{id}/edit', [FaqController::class, 'edit'])->name('admin.faqs.edit');
+        Route::post('/{id}/update', [FaqController::class, 'update'])->name('admin.faqs.update');
+        Route::get('/{id}/delete', [FaqController::class, 'delete'])->name('admin.faqs.delete');
+        Route::get('/{id}/up', [FaqController::class, 'positionUp'])->name('admin.faqs.up');
+        Route::get('/{id}/down', [FaqController::class, 'positionDown'])->name('admin.faqs.down');
+    });
 });
