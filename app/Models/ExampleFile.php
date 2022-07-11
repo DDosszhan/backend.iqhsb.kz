8632c1823103a8239fab3f97b5b7c043723f2a98
Anastas Mironov <ast@l80.ru>
2022-07-11 16:39:05 +0900

    feat: add slug to example_files

--- app/Models/ExampleFile.php
+++ app/Models/ExampleFile.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
@@ -11,11 +12,21 @@ class ExampleFile extends Model implements HasMedia
 {
     use HasFactory;
     use InteractsWithMedia;
+    use Sluggable;
 
     protected $fillable = ['name'];
 
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('default')->singleFile();
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
--- database/migrations/2022_07_11_010439_create_example_files_table.php
+++ database/migrations/2022_07_11_010439_create_example_files_table.php
@@ -11,12 +11,21 @@ return new class extends Migration
         Schema::create('example_files', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug')->unique();
             $table->timestamps();
+
+            $table->index('slug');
         });
 
         \Illuminate\Support\Facades\DB::table('example_files')->insert([
-            ['name' => 'ЭТАП 2: Запись на диагностические экзамены'],
-            ['name' => 'ЭТАП 4: Подписание договора'],
+            [
+                'name' => 'ЭТАП 2: Запись на диагностические экзамены',
+                'slug' => 'stage_2',
+            ],
+            [
+                'name' => 'ЭТАП 4: Подписание договора',
+                'slug' => 'stage_4',
+            ],
         ]);
     }
 
